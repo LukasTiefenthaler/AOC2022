cbe705390d658126c6984f779c1f9aa0dba3aaf5
LukasTiefenthaler <lukas.tiefenthaler1@gmail.com>
2022-12-01 19:28:42 +0100

    first commit2.0

---
+++ Solve.py
@@ -0,0 +1,22 @@
+import os
+
+from Days import *
+
+
+current_day = Day2
+
+input_file = "Input/" + os.path.basename(current_day.__file__.split('.')[0]) + ".txt"
+
+
+def init():
+    with open(input_file, 'r') as f:
+        solve(current_day, f.read().splitlines())
+
+
+def solve(day, _input):
+    print(f"Part1: {day.part1(_input)}")
+    print(f"Part2: {day.part2(_input)}")
+
+
+if __name__ == '__main__':
+    init()
