43c9425d8f718e55d2de3cbbee51f729a451f316
LukasTiefenthaler <lukas.tiefenthaler1@gmail.com>
2022-12-02 21:15:07 +0100

    Day2

--- Days/Day1.py
+++ Days/Day1.py
@@ -2,7 +2,7 @@ def part1(_input):
     most_calories = 0
     current_calories = 0
     for line in _input:
-        if line != "":
+        if line:
             current_calories += int(line)
         else:
             most_calories = max(current_calories, most_calories)
@@ -17,7 +17,7 @@ def part2(_input):
     most_calories = [0, 0, 0]
     current_calories = 0
     for value in _input:
-        if value != "":
+        if value:
             current_calories += int(value)
         else:
             most_calories.sort()
--- Days/Day2.py
+++ Days/Day2.py
@@ -1,6 +1,35 @@
+first = ["A", "B", "C"]
+second = ["X", "Y", "Z"]
+
+
 def part1(_input):
-    return ""
+    points = 0
+    for line in _input:
+        x, y = line.split(" ")
+        # draw
+        if first.index(x) == second.index(y):
+            points += 3
+        # win
+        elif (first.index(x) + 1) % 3 == second.index(y):
+            points += 6
+
+        points += second.index(y) + 1
+
+    return points
 
 
 def part2(_input):
-    return ""
+    points = 0
+    for line in _input:
+        x, y = line.split(" ")
+        # lose
+        if y == "X":
+            points += (first.index(x) - 1) % 3 + 1
+        # draw
+        elif y == "Y":
+            points += 3 + first.index(x) + 1
+        # win
+        else:
+            points += 6 + (first.index(x) + 1) % 3 + 1
+
+    return points
